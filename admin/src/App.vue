6f9e7345c4c7a0201c1f7bb394fe21d429599b81
Cc <599765520@qq.com>
2020-06-08 21:02:20 +0800

    1.简化登录页面代码 2.增加eslintrc.js,去掉部分检查项 解决修改代码自动编译时报错：$ is not defined

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -7,10 +7,10 @@ v<template>
             <div class="center">
               <h1>
                 <i class="ace-icon fa fa-leaf green"></i>
-                <span class="red">Ace</span>
-                <span class="white" id="id-text2">Application</span>
+                <span class="blue">控台登录</span>
+<!--                <span class="white" id="id-text2">Application</span>-->
               </h1>
-              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
+<!--              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>-->
             </div>
 
             <div class="space-6"></div>
@@ -21,7 +21,7 @@ v<template>
                   <div class="widget-main">
                     <h4 class="header blue lighter bigger">
                       <i class="ace-icon fa fa-coffee green"></i>
-                      Please Enter Your Information
+                      请输入用户信息
                     </h4>
 
                     <div class="space-6"></div>
@@ -47,7 +47,7 @@ v<template>
                         <div class="clearfix">
                           <label class="inline">
                             <input type="checkbox" class="ace" />
-                            <span class="lbl"> Remember Me</span>
+                            <span class="lbl"> 记住我</span>
                           </label>
 
                           <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
@@ -60,42 +60,42 @@ v<template>
                       </fieldset>
                     </form>
 
-                    <div class="social-or-login center">
-                      <span class="bigger-110">Or Login Using</span>
-                    </div>
+<!--                    <div class="social-or-login center">-->
+<!--                      <span class="bigger-110">Or Login Using</span>-->
+<!--                    </div>-->
 
-                    <div class="space-6"></div>
+<!--                    <div class="space-6"></div>-->
 
-                    <div class="social-login center">
-                      <a class="btn btn-primary">
-                        <i class="ace-icon fa fa-facebook"></i>
-                      </a>
+<!--                    <div class="social-login center">-->
+<!--                      <a class="btn btn-primary">-->
+<!--                        <i class="ace-icon fa fa-facebook"></i>-->
+<!--                      </a>-->
 
-                      <a class="btn btn-info">
-                        <i class="ace-icon fa fa-twitter"></i>
-                      </a>
+<!--                      <a class="btn btn-info">-->
+<!--                        <i class="ace-icon fa fa-twitter"></i>-->
+<!--                      </a>-->
 
-                      <a class="btn btn-danger">
-                        <i class="ace-icon fa fa-google-plus"></i>
-                      </a>
-                    </div>
+<!--                      <a class="btn btn-danger">-->
+<!--                        <i class="ace-icon fa fa-google-plus"></i>-->
+<!--                      </a>-->
+<!--                    </div>-->
                   </div><!-- /.widget-main -->
 
-                  <div class="toolbar clearfix">
-                    <div>
-                      <a href="#" data-target="#forgot-box" class="forgot-password-link">
-                        <i class="ace-icon fa fa-arrow-left"></i>
-                        I forgot my password
-                      </a>
-                    </div>
+<!--                  <div class="toolbar clearfix">-->
+<!--                    <div>-->
+<!--                      <a href="#" data-target="#forgot-box" class="forgot-password-link">-->
+<!--                        <i class="ace-icon fa fa-arrow-left"></i>-->
+<!--                        I forgot my password-->
+<!--                      </a>-->
+<!--                    </div>-->
 
-                    <div>
-                      <a href="#" data-target="#signup-box" class="user-signup-link">
-                        I want to register
-                        <i class="ace-icon fa fa-arrow-right"></i>
-                      </a>
-                    </div>
-                  </div>
+<!--                    <div>-->
+<!--                      <a href="#" data-target="#signup-box" class="user-signup-link">-->
+<!--                        I want to register-->
+<!--                        <i class="ace-icon fa fa-arrow-right"></i>-->
+<!--                      </a>-->
+<!--                    </div>-->
+<!--                  </div>-->
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
 
@@ -217,20 +217,20 @@ v<template>
               </div><!-- /.signup-box -->
             </div><!-- /.position-relative -->
 
-            <div class="navbar-fixed-top align-right">
-              <br />
-              &nbsp;
-              <a id="btn-login-dark" href="#">Dark</a>
-              &nbsp;
-              <span class="blue">/</span>
-              &nbsp;
-              <a id="btn-login-blur" href="#">Blur</a>
-              &nbsp;
-              <span class="blue">/</span>
-              &nbsp;
-              <a id="btn-login-light" href="#">Light</a>
-              &nbsp; &nbsp; &nbsp;
-            </div>
+<!--            <div class="navbar-fixed-top align-right">-->
+<!--              <br />-->
+<!--              &nbsp;-->
+<!--              <a id="btn-login-dark" href="#">Dark</a>-->
+<!--              &nbsp;-->
+<!--              <span class="blue">/</span>-->
+<!--              &nbsp;-->
+<!--              <a id="btn-login-blur" href="#">Blur</a>-->
+<!--              &nbsp;-->
+<!--              <span class="blue">/</span>-->
+<!--              &nbsp;-->
+<!--              <a id="btn-login-light" href="#">Light</a>-->
+<!--              &nbsp; &nbsp; &nbsp;-->
+<!--            </div>-->
           </div>
         </div><!-- /.col -->
       </div><!-- /.row -->
