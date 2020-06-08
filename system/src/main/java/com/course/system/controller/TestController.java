973a1eb2e462470e17397393011c6be2ddb9ac3a
Cc <599765520@qq.com>
2020-06-08 11:43:25 +0800

    1.system集成server模块

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.course.system.controller;
 
-import com.course.system.domain.Test;
-import com.course.system.server.TestServer;
+import com.course.server.domain.Test;
+import com.course.server.server.TestServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
