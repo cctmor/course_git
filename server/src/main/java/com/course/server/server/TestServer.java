4d2f527e77d7b3dda3cc347da708c562ecc1495a
Cc <599765520@qq.com>
2020-06-08 15:56:48 +0800

    1.Example使用示例 使用Example可以快速完成单表的增删改查

--- server/src/main/java/com/course/server/server/TestServer.java
+++ server/src/main/java/com/course/server/server/TestServer.java
@@ -1,9 +1,12 @@
 package com.course.server.server;
 
+import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class TestServer {
@@ -11,7 +14,13 @@ public class TestServer {
     @Resource
     private TestMapper testMapper;
 
-//    public List<Test> lists(){
-//      return testMapper.lists();
-//    }
+    public List<Test> lists(){
+        //查询所有
+        //return testMapper.selectByExample(null);
+        //根据条件查询
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+    }
 }
